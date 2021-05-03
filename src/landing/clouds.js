d8354d9758844a309fc247fb9f531f51015e2101
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-05-03 15:30:11 -0700

    multiplied clouds for animation

--- src/landing/clouds.js
+++ src/landing/clouds.js
@@ -3,18 +3,63 @@ import Cloud from './cloud'
 import './landing.css'
 
 export default class Clouds extends Component {
-    static initialYOffset = -20;
+    static initialYOffset = -100;
+    static animationTime = 2000;
 
-    static animationTime = 1000;
-    static cloudsPerLayer = 5;
-    static layers = 3;
+    static maxWidth = 250;
+    static minWidth = 150;
+    static maxHeight = 150;
+    static minHeight = 100;
+
+    static cloudsPerLayer = 7;
+    static layers = 4;
+
+    static sizeLayerDiff = 0.15;
+    static speedLayerDiff = 0.05;
+
+
     static loadDate = new Date();
     constructor(props) {
         super(props);
+        
+        let cloudInfo = [];
+
+        for (let i = 0; i < Clouds.layers; i++) {
+            let layer = [];
+            for (let j = 0; j < Clouds.cloudsPerLayer; j++) {
+                let width = (Math.random()*(Clouds.maxWidth- Clouds.minWidth) + Clouds.minWidth)*(1 - i*Clouds.sizeLayerDiff);
+                let height = (Math.random()*(Clouds.maxHeight - Clouds.minHeight) + Clouds.minHeight)*(1 - i*Clouds.sizeLayerDiff);
+                
+                let x = Math.random()*100;
+                let y = Math.random()*80 + 10;
+
+                for (let i = 0; i < layer.length; i++) {
+                    let distance = Math.sqrt((layer[i].x-x)**2+(layer[i].y-y)**2)
+                    if (distance > Math.max(width, height, layer[i].width, layer[i].height)) {
+                        i = -1;
+                        continue;
+                    }
+                }
+
+                layer.push({
+                    width: width,
+                    height: height,
+                    x: x,
+                    y: y,
+                    yOffset: Clouds.initialYOffset *(1 - Clouds.speedLayerDiff*i),
+                    opacity: Cloud.opacity*(1 - i*.1)
+                })
+            }
+            cloudInfo.push(layer);
+        }
+
+        console.log(cloudInfo)
+
         this.state = {
-            x: 10,
-            y: 50,
-            yOffset: Clouds.initialYOffset,
+            cloudInfo: cloudInfo
+            // x: 10,
+            // y: 50,
+            // yOffset: Clouds.initialYOffset,
         }
         this.interval = 0;
     }
@@ -25,6 +70,9 @@ export default class Clouds extends Component {
         //     x: e.clientX/window.innerWidth * 100, 
         //     y: 100 - e.clientY/window.innerHeight * 100})
         // console.log(this.state.y)
+        console.log(this.state.cloudInfo.map(layer => layer.map(
+            cloudInfo => cloudInfo )
+        ))
     }
 
     animate = () => {
@@ -34,11 +82,19 @@ export default class Clouds extends Component {
             clearInterval(this.interval);
             return;
         }
+        let newState = {...this.state};
         let yOffset = (Clouds.initialYOffset)*(1 - timePercent);
+        for (let i = 0; i < Clouds.layers; i++) {
+            for (let j = 0; j < Clouds.cloudsPerLayer; j++) {
+                newState.cloudInfo[i][j].yOffset = yOffset*(1 - i*Clouds.speedLayerDiff)
+            }
+        }
+        
         this.setState({
-            x: this.state.x, 
-            yOffset: yOffset})
-        console.log(yOffset)
+            newState
+        });
+            
+        // console.log(yOffset)
     }
 
     componentDidMount() {
@@ -53,7 +109,11 @@ export default class Clouds extends Component {
     render() {
         return (
             <div id="clouds" onClick={this.mouseEvent}>
-                <Cloud x={Math.round(this.state.x)} y={this.state.y + this.state.yOffset} width="100" height="100" />
+                {this.state.cloudInfo.map(layer => layer.map(
+                    cloudInfo => 
+                    <Cloud key={cloudInfo.id} x={Math.round(cloudInfo.x)} y={cloudInfo.y + cloudInfo.yOffset} width={cloudInfo.width} height={cloudInfo.height} />)
+                )}
+                
             </div>
         )
     }
