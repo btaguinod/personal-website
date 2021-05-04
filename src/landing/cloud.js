b029a341e65a11ac3a49877c25f767583b1932d8
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-05-04 12:40:00 -0700

    added hover animation

--- src/landing/cloud.js
+++ src/landing/cloud.js
@@ -1,41 +1,112 @@
 import React, { Component } from 'react'
 
 export default class Cloud extends Component {
+    static hoverAnimSpeed = 1;
+
     constructor(props) {
         super(props);
         let x = this.props.x-.5*this.props.width/window.innerWidth*100
         let y = this.props.y-.5*this.props.height/window.innerHeight*100
         this.state = {
+            offsets: {
+                widthOffset: 0,
+                heightOffset: 0,
+                opacityOffset: 0
+            },
             style: {
-                width: this.props.width+'px',
-                height: this.props.height+'px',
-                left: x +'vw',
-                bottom: y+'vh',
-                opacity: this.props.opacity.toString()
+                width: this.props.width,
+                height: this.props.height,
+                left: x,
+                bottom: y,
+                opacity: this.props.opacity
             }
         }
     }
+
     componentDidUpdate(prevProps) {
         if (this.props !== prevProps) {
-            let x = this.props.x-.5*this.props.width/window.innerWidth*100
-            let y = this.props.y-.5*this.props.height/window.innerHeight*100
+            let trueWidthPercent = (this.props.width + this.state.offsets.widthOffset)/window.innerWidth
+            let trueHeightpercent = (this.props.height + this.state.offsets.heightOffset)/window.innerHeight
+
+            let x = this.props.x-.5*trueWidthPercent*100
+            let y = this.props.y-.5*trueHeightpercent*100
+
+            let newState = {
+                offsets:{...this.state.offsets}, 
+                style:{...this.state.style}
+            };
             
-            this.setState({
-                style: {
-                    width: this.props.width+'px',
-                    height: this.props.height+'px',
-                    left: x+'vw',
-                    bottom: y+'vh',
-                    opacity: this.props.opacity.toString()
-                }
-            })
+            newState.style.left = x;
+            newState.style.bottom = y;
+            this.setState(
+                newState
+            )
         }
     }
+
+    mouseEnter = e => {
+        clearInterval(this.shrinkInterval)
+        this.growInterval = setInterval(() => {
+            let newState = {
+                offsets: {...this.state.offsets},
+                style: {...this.state.style}
+            }
+
+            if (this.state.style.opacity + newState.offsets.opacityOffset >= 1) {
+                clearInterval(this.growInterval)
+                return;
+            }
+
+            newState.offsets.widthOffset += 0.1;
+            newState.offsets.heightOffset += 0.1;
+            newState.offsets.opacityOffset += 0.01;
+
+            this.setState(newState);
+        }, 1)
+    }
+
+    mouseLeave = e => {
+            clearInterval(this.growInterval)
+            this.shrinkInterval = setInterval(() => {
+            let newState = {
+                offsets: {...this.state.offsets},
+                style: {...this.state.style}
+            }
+
+            if (newState.offsets.opacityOffset <= 0) {
+                clearInterval(this.shrinkInterval)
+                return
+            }
+
+            newState.offsets.widthOffset -= 0.1;
+            newState.offsets.heightOffset -= 0.1;
+            newState.offsets.opacityOffset -= 0.01;
+
+            this.setState(newState);
+        }, 1)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.growInterval)
+        clearInterval(this.shrinkInterval)
+    }
+
     render() {
+        let style = {...this.state.style}
+
+        style.width = (style.width + this.state.offsets.widthOffset) + 'px';
+        style.height = (style.height + this.state.offsets.heightOffset) + 'px';
+        style.left = style.left + 'vw';
+        style.bottom = style.bottom + 'vh';
+        style.opacity = (style.opacity + this.state.offsets.opacityOffset).toString();
+
         return (
-            <div className="cloud" style={this.state.style}>
-                
-            </div>
+            <div 
+                onMouseEnter={this.mouseEnter} 
+                onMouseLeave={this.mouseLeave}
+                className="cloud" 
+                style={style} 
+            />
         )
     }
 }
--- src/landing/clouds.js
+++ src/landing/clouds.js
@@ -3,8 +3,10 @@ import Cloud from './cloud'
 import './landing.css'
 
 export default class Clouds extends Component {
-    static initialYOffset = -100;
-    static animationTime = 2000;
+    static initialYOffset = -200;
+    static appearTime = 250;
+    static openAnimTime = 1750;
+    static idleSpeed = 1;
 
     static maxWidth = 250;
     static minWidth = 150;
@@ -13,65 +15,62 @@ export default class Clouds extends Component {
 
     static opacity = 0.9;
 
-    static cloudsPerLayer = 5;
+    static totalClouds = 50;
     static layers = 5;
 
     static speedLayerDiff = 0.1;
-    static sizeLayerDiff = 0.1;
-    static opacityLayerDiff = 0.1;
+    static sizeLayerDiff = 0.075;
+    static opacityLayerDiff = 0.2;
 
-
-    static loadDate = new Date();
     constructor(props) {
         super(props);
         
+        this.stage = 0;
+
         let cloudInfo = [];
 
-        for (let i = 0; i < Clouds.layers; i++) {
-            for (let j = 0; j < Clouds.cloudsPerLayer; j++) {
-                let width = (Math.random()*(Clouds.maxWidth- Clouds.minWidth) + Clouds.minWidth)*(1 - i*Clouds.sizeLayerDiff);
-                let height = (Math.random()*(Clouds.maxHeight - Clouds.minHeight) + Clouds.minHeight)*(1 - i*Clouds.sizeLayerDiff);
-                
-                let x = Math.random()*100;
-                let y = Math.random()*80 + 10;
+        for (let i = 0; i < Clouds.totalClouds; i++) {
+            let layer = Math.floor(Math.random()*Clouds.layers);
 
-                for (let k = 0; k < cloudInfo.length; k++) {
-                    let percentX = cloudInfo[k].x-x;
-                    let percentY = cloudInfo[k].y-y;
-                    let distance = Math.sqrt((percentX*window.innerWidth*.01)**2+(percentY*window.innerHeight*.01)**2)
+            let width = (Math.random()*(Clouds.maxWidth - Clouds.minWidth) + Clouds.minWidth)*(1 - layer*Clouds.sizeLayerDiff);
+            let height = (Math.random()*(Clouds.maxHeight - Clouds.minHeight) + Clouds.minHeight)*(1 - layer*Clouds.sizeLayerDiff);
+            
+            let x = Math.random()*100;
+            let y = Math.random()*80 + 10;
 
-                    if (distance < Math.min(width, height, cloudInfo[k].width, cloudInfo[k].height)) {
-                        x = Math.random()*100;
-                        y = Math.random()*80 + 10;
+            for (let k = 0; k < cloudInfo.length; k++) {
+                let xPercent = cloudInfo[k].x-x;
+                let yPercent = cloudInfo[k].y-y;
+                let distance = Math.sqrt((xPercent*window.innerWidth*.01)**2+(yPercent*window.innerHeight*.01)**2)
 
-                        k = -1;
-                        continue;
-                    }
+                if (distance < Math.min(width, height, cloudInfo[k].width, cloudInfo[k].height)) {
+                    x = Math.random()*100;
+                    y = Math.random()*80 + 10;
+
+                    k = -1;
+                    continue;
                 }
-
-                console.log(Clouds.opacity*(1 - Clouds.opacityLayerDiff*i))
-
-                cloudInfo.push({
-                    layer: i,
-                    width: width,
-                    height: height,
-                    x: x,
-                    y: y,
-                    yOffset: Clouds.initialYOffset *(1 - Clouds.speedLayerDiff*i),
-                    opacity: Clouds.opacity*(1 - Clouds.opacityLayerDiff*i)
-                })
             }
-        }
 
-        console.log(cloudInfo)
+            // console.log(Clouds.opacity*(1 - Clouds.opacityLayerDiff*layer))
+
+            cloudInfo.push({
+                layer,
+                width,
+                height,
+                x,
+                y,
+                yAnimOffset: Clouds.initialYOffset *(1 - Clouds.speedLayerDiff*layer),
+                opacity: Clouds.opacity*(1 - Clouds.opacityLayerDiff*layer)
+            })
+
+        }
 
         this.state = {
             cloudInfo: cloudInfo
-            // x: 10,
-            // y: 50,
-            // yOffset: Clouds.initialYOffset,
         }
-        this.interval = 0;
+
+
     }
 
     mouseEvent = e => {
@@ -79,43 +78,64 @@ export default class Clouds extends Component {
         console.log(this.state.cloudInfo[0])
     }
 
-    animate = () => {
-        let end = (Date.now() - Clouds.loadDate.getTime()) > Clouds.animationTime;
-        let timePercent = (Date.now() - Clouds.loadDate.getTime())/Clouds.animationTime
-        if (end) {
-            clearInterval(this.interval);
-            return;
-        }
+    openAnimation = () => {
+        let timePercent = (Date.now() - this.startAnimDate.getTime())/Clouds.openAnimTime
+
         let newState = {...this.state};
+
         // Linear version
         // let yOffset = (Clouds.initialYOffset)*(1 - timePercent);
 
-        let yOffset = ((timePercent - 1)**2)*Clouds.initialYOffset;
+        let yAnimOffset = ((timePercent - 1)**2)*Clouds.initialYOffset;
         for (let cloud of newState.cloudInfo) {
-            cloud.yOffset = yOffset*(1 - cloud.layer*Clouds.speedLayerDiff)
+            cloud.yAnimOffset = yAnimOffset*(1 - cloud.layer*Clouds.speedLayerDiff)
         }
         
-        this.setState({
-            newState
-        });
-            
-        // console.log(yOffset)
+        this.setState(newState);
+    }
+
+    idleAnimation = () => {
+        let newState = {...this.state};
+
+        for (let cloud of newState.cloudInfo) {
+            let x = (cloud.x + Clouds.idleSpeed*(1 - cloud.layer*Clouds.speedLayerDiff)*0.01)
+            if (x > 100 + 100*cloud.width/(2*window.innerWidth)) {
+                x = -100*cloud.width/(2*window.innerWidth)
+            }
+            cloud.x = x
+        }
+
+        this.setState(newState);
     }
 
     componentDidMount() {
         console.log('mount')
-        this.interval = setInterval(this.animate, 1);
+        setTimeout(() => {
+            this.startAnimDate = new Date();
+            this.openAnimInterval = setInterval(this.openAnimation, 1);
+            setTimeout(() => {clearInterval(this.openAnimInterval);}, Clouds.openAnimTime);
+        }, Clouds.appearTime)
+        this.idleAnimInterval = setInterval(this.idleAnimation, 1);
+
     }
     componentWillUnmount() {
         console.log('unmount')
-        clearInterval(this.interval);
+        clearInterval(this.openAnimInterval);
+        clearInterval(this.idleAnimInterval);
     }
 
     render() {
         return (
             <div id="clouds" onClick={this.mouseEvent}>
                 {this.state.cloudInfo.map(cloudInfo =>
-                    <Cloud key={cloudInfo.id} x={Math.round(cloudInfo.x)} y={cloudInfo.y + cloudInfo.yOffset} width={cloudInfo.width} height={cloudInfo.height} opacity={cloudInfo.opacity}/>
+                    <Cloud 
+                        key={cloudInfo.id} 
+                        x={cloudInfo.x} 
+                        y={cloudInfo.y + cloudInfo.yAnimOffset} 
+                        width={cloudInfo.width} 
+                        height={cloudInfo.height} 
+                        opacity={cloudInfo.opacity}
+                    />
                 )}
                 
             </div>
