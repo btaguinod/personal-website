f189ef1b9919476c804edb7d1d064b36f99aaba4
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-05-07 14:36:32 -0700

    removed cards

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import Projects from './projects/projects';
 import Courses from './courses/courses';
 import './index.css';
 import Background from './background/background';
+import Landing from './landing/landing';
 
 ReactDOM.render(
   <React.StrictMode>
     <Background>
+      <Landing />
       <About />
-      <About />
+      <Projects />
+      <Courses />
     </ Background>
   </ React.StrictMode>,
   document.getElementById('root')
