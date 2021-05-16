231e6f46ba059da1a7e1ddbc05b8abf10a281b0e
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-05-16 06:42:13 -0700

    completely reset design

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import About from './about/about';
-import Projects from './projects/projects';
-import Courses from './courses/courses';
 import './index.css';
-import Background from './background/background';
-import Landing from './landing/landing';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Background>
-      {/* <Landing />
-      <About />
-      <Projects />
-      <Courses /> */}
-    </ Background>
+    
   </ React.StrictMode>,
   document.getElementById('root')
 );
