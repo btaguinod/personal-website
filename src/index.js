6a757214c1f705ae3250a61e1cf2406a61778628
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-08-05 16:42:54 -0700

    Remove sidelinks on mobile and Fix resizing

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import About from './About/About';
 import Projects from './Projects/Projects';
 import Courses from './Courses/Courses';
 
-let vh = window.innerHeight*0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`)
+const setHeight = () => {
+  let vh = window.innerHeight*0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+window.addEventListener('resize', setHeight);
+setHeight();
 
 ReactDOM.render(
   <React.StrictMode>
