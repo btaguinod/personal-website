aa8e519f046fdf6e6ff975373731b480dabeaac6
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-08-05 17:56:03 -0700

    Remove js resizing

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ import About from './About/About';
 import Projects from './Projects/Projects';
 import Courses from './Courses/Courses';
 
-const setHeight = () => {
-  let vh = window.innerHeight*0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-}
-window.addEventListener('resize', setHeight);
-setHeight();
-
 ReactDOM.render(
   <React.StrictMode>
     <Header />
