ebeb9d8299753df6d58f3ea8657bc03cdde0f7e5
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-08-14 13:39:42 -0700

    Put back courses highlight

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -6,7 +6,7 @@ export default function Courses() {
     return (
         <div className="page" id="courses">
             <div id="courses-title">
-                <div className="title"> University Courses</div>
+                <div className="title"> University <a href="#courses" className="highlight"> Courses </a></div>
             </div>
             <CourseCards />
         </div>
