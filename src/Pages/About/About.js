836bb4d2010cfa637c34d664e2280debdd5c0d43
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-09-14 09:00:26 -0700

    Update Placesoft status

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <div className="page" id="about">
             <div className="page-text" id="about-text">
                 <div className="title">Hi, I'm <a href="https://www.linkedin.com/in/benedict-taguinod/" className="highlight"> Benedict </a></div>
-                <div className="subtext-2">I'm an EECS student at <span className="highlight"><a href="https://www.linkedin.com/school/uc-berkeley/"> UC Berkeley</a></span> and a software developer at <span className="highlight"><a href="https://www.linkedin.com/company/placesoft/about/"> Placesoft </a></span></div>
+                <div className="subtext-2">I'm an EECS student at <span className="highlight"><a href="https://www.linkedin.com/school/uc-berkeley/"> UC Berkeley</a></span> and a former software developer at <span className="highlight"><a href="https://www.linkedin.com/company/placesoft/about/"> Placesoft </a></span></div>
                 <div className="subtext-1">Check out my work below!</div>
             </div>
             <a id="arrow" href="#projects" className="icon"><FontAwesomeIcon icon={faArrowCircleDown} /></a>
