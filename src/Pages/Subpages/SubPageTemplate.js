5a39651cee10da716ecbc2f90fce17a13bbafdc7
Benedict Taguinod <benedict.a.taguinod@gmail.com>
2023-01-22 21:02:33 -0800

    add sub website

---
+++ src/Pages/Subpages/SubPageTemplate.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './SubPageTemplate.css';
+import { StlViewer } from "react-stl-viewer";
+
+export default function SubPage({title, description, cards}) {
+  return (
+    <div className="subpage page">
+      <div className="subpage-title">
+        <div className="title">{title}</div>
+      </div>
+      <div className="subpage-description">
+        <div className="subtext-2">{description}</div>
+      </div>
+      <div className="subpage-cards">
+        {cards.map((card, i) => <SubPageCard index={i} key={i} card={card}/>)}
+      </div>
+    </div>
+  )
+}
+
+
+function SubPageCard({index, card}) {
+  let content;
+  if (card.type === 'model') {
+    content = (
+      <StlViewer
+        className="subpage-card-content-inner"
+        orbitControls
+        shadows
+        url={card.content}
+      />
+    );
+  }
+  return (
+    <div className="subpage-card">
+      <div className="subpage-card-content">
+        {content}
+      </div>
+      <div className="subpage-card-text">
+        <div className="subpage-card-title">{card.title}</div>
+        <div className="subpage-card-description">{card.description}</div>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import Header from './Header/Header.js';
 import SideLinks from './SideLinks/SideLinks';
 import About from './Pages/About/About';
 import Projects from './Pages/Projects/Projects';
 import Courses from './Pages/Courses/Courses';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Models from './Pages/Subpages/Models/Models';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <Header />
-    <SideLinks />
-    <About />
-    <Projects />
-    <Courses />
-    <div id="bar"/>
-  </ React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<MainPage />} />
+          <Route path='/models' element={<Models />}/>
+        </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+function MainPage() {
+  return (
+    <div id="mainpage">
+      <Header />
+      <SideLinks />
+      <About />
+      <Projects />
+      <Courses />
+      <div id="bar"/>
+    </div>
+  );
+}
